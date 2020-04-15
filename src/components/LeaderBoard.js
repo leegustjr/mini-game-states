3a2a6efddb8e7a6abf1a5b1ff079df16155589aa
Simon Lee <simon@LEEui-MacBookPro.local>
2020-04-15 13:23:13 +0900

    :ambulance: fix leaderboard func
     - refactoring leaderboard function

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -11,38 +11,68 @@ axios.defaults.withCredentials = true;
 class LeaderBoard extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      game1Score: ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+      game2Score: ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+      game3Score: ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    };
   }
 
-  // makeTable() {}
-
   componentDidMount() {
+    console.log(this.props);
     const config = {
       headers: { Authorization: this.props.token.accessToken },
     };
 
     axios
-      .get('http://13.209.41.64:4100/scores/scores', config)
+      .post('http://14.41.86.57:4100/scores/scores', config)
       .then((res) => {
         for (let i = 0; i < res.data.games.length; i += 1) {
           res.data.games[i].scores.sort((a, b) => b.place - a.place);
         }
-        console.log(res.data.games);
-        this.props.getMyScore(res.data);
+        this.props.changeMyScore(res.data);
       })
-      .then()
-      .catch((err) => console.log(err));
+      .catch((err) => console.log('요긴가?', err));
+  }
 
-    const obj = {};
-    obj.gameTitle = '';
-
-    axios
-      .get('http://13.209.41.64:4100/scores/leaderboard', obj)
+  async getGame1Score() {
+    console.log('hello');
+    await axios
+      .post('http://14.41.86.57:4100/scores/leaderboard', {
+        gameTitle: 'Game 1',
+      })
       .then((res) => {
-        res.data.leaderboard.sort((a, b) => a.place - b.place);
-        this.props.changeLeaderBoard(res.data);
+        console.log('success');
         console.log(res);
-        // / (res) =>/
+        // res.data.leaderboard.map((val) => (
+        //   <tr>
+        //     <td>{val.nickname}</td>
+        //     <td>{val.scores}</td>
+        //   </tr>
+        // ));
+        this.setState({ game1Score: res.data.leaderboard });
+      })
+      .catch((error) => console.log('여기야 여기', error.response));
+  }
+
+  async getGame2Score() {
+    await axios
+      .post('http://13.209.41.64:4100/scores/leaderboard', {
+        gameTitle: 'Game 2',
+      })
+      .then((res) => {
+        this.setState({ game2Score: res.data.leaderboard });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  async getGame3Score() {
+    await axios
+      .post('http://13.209.41.64:4100/scores/leaderboard', {
+        gameTitle: 'Game 3',
+      })
+      .then((res) => {
+        this.setState({ game3Score: res.data.leaderboard });
       })
       .catch((err) => console.log(err));
   }
@@ -69,81 +99,51 @@ class LeaderBoard extends Component {
               <div className="row">
                 <div className="col-md-12">
                   <section id="display">
-                    <div className="table-responsive" id="game1">
+                    <div className="table-responsive" id="num">
                       <table className="table">
                         <thead>
                           <tr>
+                            <th>Game</th>
+                          </tr>
+                          <tr>
                             <th>*</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((val) => (
+                            <tr key={val}>
+                              <td>{val}</td>
+                            </tr>
+                          ))}
+                          <tr>
+                            <td>My Score</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <div className="table-responsive" id="table1">
+                      <table className="table">
+                        <thead>
+                          <tr>
+                            <th colSpan="2">Game1</th>
+                          </tr>
+                          <tr>
                             <th>nickname</th>
                             <th>score</th>
                           </tr>
                         </thead>
                         <tbody>
+                          {console.log(this.state.game1Score)}
+                          {this.state.game1Score.map((val) => (
+                            <tr>
+                              <td>{val}</td>
+                              <td>{val}</td>
+                            </tr>
+                          ))}
                           <tr>
-                            <td>1</td>
-                            <td>멋쟁이산</td>
-                            <td>100</td>
-                            <td />
-                          </tr>
-                          <tr>
-                            <td>2</td>
-                            <td></td>
-                            <td></td>
-                            <td />
-                          </tr>
-                          <tr>
-                            <td>3</td>
-                            <td></td>
-                            <td></td>
-                            <td />
-                          </tr>
-                          <tr>
-                            <td>4</td>
-                            <td></td>
-                            <td></td>
-                            <td />
-                          </tr>
-                          <tr>
-                            <td>5</td>
-                            <td></td>
-                            <td></td>
-                            <td />
-                          </tr>
-                          <tr>
-                            <td>6</td>
-                            <td></td>
-                            <td></td>
-                            <td />
-                          </tr>
-                          <tr>
-                            <td>7</td>
-                            <td></td>
-                            <td></td>
-                            <td />
-                          </tr>
-                          <tr>
-                            <td>8</td>
-                            <td></td>
-                            <td></td>
-                            <td />
-                          </tr>
-                          <tr>
-                            <td>9</td>
-                            <td></td>
-                            <td></td>
-                            <td />
-                          </tr>
-                          <tr>
-                            <td>10</td>
-                            <td></td>
-                            <td></td>
-                            <td />
-                          </tr>
-                          <tr>
-                            <td>My Score</td>
-                            <td>{this.props.myScore.nickname}</td>
-                            <td>{this.props.myScore.games[0].scores[0]}</td>
-                            <td />
+                            <td colSpan="2">
+                              {this.props.myScore.games[1].scores[0]}
+                            </td>
                           </tr>
                         </tbody>
                       </table>
@@ -152,55 +152,24 @@ class LeaderBoard extends Component {
                       <table className="table">
                         <thead>
                           <tr>
+                            <th colSpan="2">Game2</th>
+                          </tr>
+                          <tr>
                             <th>nickname</th>
                             <th>score</th>
                           </tr>
                         </thead>
                         <tbody>
+                          {this.state.game2Score.map((val) => (
+                            <tr>
+                              <td>{val}</td>
+                              <td>{val}</td>
+                            </tr>
+                          ))}
                           <tr>
-                            <td>멋쟁이산</td>
-                            <td>100</td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td>{this.props.myScore.nickname}</td>
-                            <td>{this.props.myScore.games[1].scores[0]}</td>
+                            <td colSpan="2">
+                              {this.props.myScore.games[1].scores[0]}
+                            </td>
                           </tr>
                         </tbody>
                       </table>
@@ -209,54 +178,24 @@ class LeaderBoard extends Component {
                       <table className="table">
                         <thead>
                           <tr>
+                            <th colSpan="2">Game3</th>
+                          </tr>
+                          <tr>
                             <th>nickname</th>
                             <th>score</th>
                           </tr>
                         </thead>
                         <tbody>
+                          {this.state.game3Score.map((val) => (
+                            <tr>
+                              <td>{val}</td>
+                              <td>{val}</td>
+                            </tr>
+                          ))}
                           <tr>
-                            <td>멋쟁이산</td>
-                            <td>100</td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                          <tr>
-                            <td>{this.props.myScore.nickname}</td>
-                            <td>{this.props.myScore.games[2].scores[0]}</td>
+                            <td colSpan="2">
+                              {this.props.myScore.games[2].scores[0]}
+                            </td>
                           </tr>
                         </tbody>
                       </table>
@@ -271,5 +210,4 @@ class LeaderBoard extends Component {
     );
   }
 }
-
 export default withRouter(LeaderBoard);
